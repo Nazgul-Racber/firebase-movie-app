657694b0385dc9ae7c90846bb061d1e7fd05c7e4
email <racbernazgul@gmail.com>
2023-08-01 14:16:10 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppRouter from "./router/AppRouter";
+
+const App = () => {
+  return (
+    <div className="dark:bg-gray-dark-main min-h-screen">
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/auth/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+//* https://firebase.google.com/docs/auth/web/start
+//* https://console.firebase.google.com/ => project settings
+//! firebase console settings bölümünden firebaseconfig ayarlarını al
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
